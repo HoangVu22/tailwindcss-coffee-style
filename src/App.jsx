56922b694ebea76487c5dccda6fb5911e2e35234
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2023-11-23 09:52:27 +0700

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-// import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <h1 className="text-red-500">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
