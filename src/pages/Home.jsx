f5bde0f8b0f75045970307835361d40e80570585
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2023-11-25 16:27:29 +0700

    Make Featured mugs section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
             <div className="font-medium text-5xl mb-6">Coffee Mugs</div>
             <div className="font-medium text-lg mb-8">The most versatile furniture system ever created. Designed to fit your life, made to move and grow.</div>
             <div className="flex justify-center ">
-              <div className="uppercase bg-white text-gray-950 w-max tracking-widest py-4 px-6 text-sm font-semibold rounded-md cursor-pointer hover:bg-opacity-95">
+              <div className="nhv-button bg-white text-gray-950 w-max rounded-md">
                 Explore our products
               </div>
             </div>
@@ -21,7 +21,7 @@ function Home() {
         </div>
       </div>
 
-      <div className="story w-full h-full flex justify-center items-center">
+      <div className="story flex justify-center items-center">
         <div className="px-4 sm:px-16 md:px-32 lg:px-64 xl:px-72 py-32 text-center">
           <div className="text-3xl mb-6 leading-10 text-gray-900">Even the all-powerful Pointing has no control about the blind texts.</div>
           <div className="text-gray-500 mb-6 leading-7">It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</div>
@@ -36,8 +36,45 @@ function Home() {
         <div className="nhv-subheading-label">Featured mugs</div>
         <div className="nhv-subheading-deco-line"></div>
       </div>
-      <div className="featured-mugs">
-        Featured mugs
+      <div className="featured-mugs w-[95%] lg:w-[70%] mx-auto mb-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 gap-10">
+          <div className="nhv-product-cart">
+            <div className="h-[400px] sm:h-[500px] bg-[url('./public/images/featured-mugs-01.jpg')] bg-cover bg-no-repeat bg-center">
+              <a href="#" alt="Featured-mugs">
+                <div className="w-full h-full hover:bg-gray-900 hover:bg-opacity-10 hover:transition-all hover:ease-in-out hover:duration-300 relative group">
+                  <div className="nhv-button absolute bg-white text-gray-950 w-11/12 bottom-4 left-1/2 -translate-x-1/2 hidden group-hover:block group-hover:animate-fadeIn">Explore mug</div>
+                </div>
+              </a>
+            </div>
+            <div className="nhv-product-info text-center mt-8">
+              <a href="" alt="Product-name">
+                <div className="text-xl mb-2 font-medium hover:text-coffee-400">Pink Premium Ceramic</div>
+              </a>
+              <div>
+                <span className='text-gray-400'>$99.00 USD</span>
+              </div>
+            </div>
+          </div>
+          <div className="nhv-product-cart">
+            <div className="h-[400px] sm:h-[500px] bg-[url('./public/images/featured-mugs-02.jpg')] bg-cover bg-no-repeat bg-center">
+              <a href="#" alt="Featured-mugs">
+                <div className="w-full h-full hover:bg-gray-900 hover:bg-opacity-10 hover:transition-all hover:ease-in-out hover:duration-300 relative group">
+                  <div className="absolute w-[100px] bg-white py-2 px-4 top-3 right-3 text-center font-semibold text-coffee-400 rounded-md">On Sale.</div>
+                  <div className="nhv-button absolute bg-white text-gray-950 w-11/12 bottom-4 left-1/2 -translate-x-1/2 hidden group-hover:block group-hover:animate-fadeIn">Explore mug</div>
+                </div>
+              </a>
+            </div>
+            <div className="text-center mt-8">
+              <a href="" alt="Product-name">
+                <div className="text-xl mb-2 font-medium hover:text-coffee-400">Golden Designers Mug</div>
+              </a>
+              <div>
+                <span className="text-xl text-coffee-400 font-medium">$50.00</span>
+                <span className='ml-2 text-gray-400 line-through'>$69.00 USD</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="nhv-subheading">
